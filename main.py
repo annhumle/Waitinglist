ea9f2781bc690512f5c1dcd0e30d8cb46b6e1ac2
kristoffer <kristoffer@teletronics.ae>
2024-03-04 23:54:18 +0400

    Updating with check for status code

--- main.py
+++ main.py
@@ -10,34 +10,39 @@ last_send_time = []
 
 def is_open():
     page = requests.get("https://findbolig.nu/da-dk/udlejere")
+    if not page.status_code == 200:
+        if check_if_sms(3600):
+            send_sms(["4542436150"], "Shit, vi er blokeret!")
+            last_send_time.append(datetime.now()) 
+        
+        return
+
     soup = BeautifulSoup(page.content, "html.parser")
     tds = soup.find_all("td")[2:]
     status = tds[1::2]
     for s in status:
         if not "Lukket" in s:
             return True
-    return False
+    return True
 
 def job():
     if is_open():
         logging.info("List is open")
-        if check_if_sms():
-            send_sms()
+        if check_if_sms(120):
+            send_sms(["4542436150", "4531450399", "4552115035", "4531446415"], "Ventelisten er åben")
             last_send_time.append(datetime.now()) 
     else:
         logging.info("list is closed")
 
-def check_if_sms():
-    return not len(last_send_time) or (datetime.now() - last_send_time[-1]).seconds > 120
+def check_if_sms(delay):
+    return not len(last_send_time) or (datetime.now() - last_send_time[-1]).seconds > delay
     
-def send_sms():
+def send_sms(recipients, message):
     token="sbQJTYgFQue-5pNajzisBWwQdStSacAjsvuOK5I1twSTVkAGkCGInaDGlL5tEWq6"
     
-    recipients = ["4542436150", "4531450399", "4552115035", "4531446415"]
-
     payload = {
         "sender": "Ventelisten",
-        "message": "Ventelisten er åben!",
+        "message": message,
         "recipients": [
             {"msisdn": recipient_number}
             for recipient_number in recipients
